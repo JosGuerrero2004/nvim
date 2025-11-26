5df60d1fddc4935be388ea0fe649520e3b87fc5c
Josias <josiasguerrero2004@gmail.com>
2025-11-26 11:40:15 -0600

    feat: winborders

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      views = {
+        hover = {
+          border = {
+            style = "rounded",
+          },
+          position = {
+            row = 2, -- desplaza hacia abajo
+            col = 0, -- alineado a la izquierda del cursor
+          },
+        },
+      },
+    },
+  },
+}
