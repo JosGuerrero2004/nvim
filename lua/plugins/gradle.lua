1b1c77eb21799a65f0dfd34250f10530d259abbd
Josias <josiasguerrero2004@gmail.com>
2026-09-07 23:12:17 -0600

    clang and gradle supp

--- lua/plugins/gradle.lua
+++ lua/plugins/gradle.lua
@@ -14,4 +14,21 @@ return {
   --   },
   -- },
   "oclay1st/maven.nvim",
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        java = { "google-java-format" },
+      },
+    },
+  },
+
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "google-java-format" })
+    end,
+  },
 }
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -14,4 +14,43 @@ return {
   { "mlaursen/vim-react-snippets" },
   { "mbbill/undotree" },
   -- { "dcampos/nvim-snippy", dependencies = { "honza/vim-snippets" } },
+
+  {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          lua_ls = {
+            settings = {
+              Lua = {
+                workspace = {
+                  library = {
+                    vim.env.VIMRUNTIME,
+                    "/usr/share/hypr/stubs", -- Hyprland stubs
+                  },
+                  checkThirdParty = false,
+                },
+                diagnostics = {
+                  globals = { "vim" },
+                },
+              },
+            },
+          },
+          cssls = {},
+          html = {},
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
+          "css",
+          "scss",
+        })
+      end
+    end,
+  },
 }
